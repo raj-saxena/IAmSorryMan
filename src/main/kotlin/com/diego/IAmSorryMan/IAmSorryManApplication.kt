b12aea32073edacf14a20489f1c424f2158f0ab1
Bart <bartosz.kaszewczuk@icloud.com>
2017-10-13 13:34:02 +0200

    #5 Migrate Java project to Kotlin

---
+++ src/main/kotlin/com/diego/IAmSorryMan/IAmSorryManApplication.kt
@@ -0,0 +1,11 @@
+package com.diego.IAmSorryMan
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class IAmSorryManApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(IAmSorryManApplication::class.java, *args)
+}
