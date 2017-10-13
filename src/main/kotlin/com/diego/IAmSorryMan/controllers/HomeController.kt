d0fb504e2a2bf6ba0ea8cbbda3bd369a308a0719
Bart <bartosz.kaszewczuk@icloud.com>
2017-10-13 15:03:09 +0200

    #2 Create an endpoint to get sorry messages

---
+++ src/main/kotlin/com/diego/IAmSorryMan/controllers/HomeController.kt
@@ -0,0 +1,23 @@
+package com.diego.IAmSorryMan.controllers;
+
+import com.diego.IAmSorryMan.models.SorryMessage
+import com.diego.IAmSorryMan.services.SorryMessagesService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @Autowired
+    private lateinit var sorryMessagesService : SorryMessagesService
+
+    @GetMapping("/")
+    fun getSorryMessage() : ResponseEntity<SorryMessage>  {
+        return ResponseEntity(sorryMessagesService.getRandom(), HttpStatus.OK)
+    }
+
+}
